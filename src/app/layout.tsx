a06a80879eb19517e7e41f2348ee2f36387e0d71
Serghei Filimonov <filimonov.sergey@gmail.com>
2025-12-04 11:13:10 +0200

    New app logo and favicons. Date field fix on mobile.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,21 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   ),
-  icons: { icon: "/favicon.ico" },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
   robots: { index: true, follow: true },
 };
 
